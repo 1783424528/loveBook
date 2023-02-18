cdd6814a7b8572f7ca538f90b58415c91adb4dcb
xiaolishifu <1783424528@qq.com>
2023-02-18 15:53:47 +0800

    添加登录

--- miniprogram/pages/usercenter/index.js
+++ miniprogram/pages/usercenter/index.js
@@ -5,16 +5,43 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+	userInfo: '', //用于存放获取的用户信息
   },
 
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-
-  },
+  onLoad(){
+	let user = wx.getStorageSync('user')
+	this.setData({
+	  userInfo: user
+	})
+},
+    // 授权登录
+    login() {
+        wx.getUserProfile({
+            desc: '必须授权才能继续使用', // 必填 声明获取用户个人信息后的用途，后续会展示在弹窗中
+            success:(res)=> { 
+                console.log('授权成功', res);
+                wx.setStorageSync('user',res.userInfo)
+                this.setData({ 
+                    userInfo:res.userInfo
+                })
+            },
+            fail:(err)=> {
+                console.log('授权失败', err);
+            }
+        })
+    },
+    // 退出登录
+    loginOut(){
+        this.setData({ 
+            userInfo:''
+        })
+        // 清空缓存
+        wx.setStorageSync('user',null)
+    },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
